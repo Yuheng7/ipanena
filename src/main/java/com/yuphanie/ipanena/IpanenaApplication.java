f9e0593eb106e3fd9fb6ca984806c6889096cf28
Yuheng Wang <yuhengwang@Yuhengs-MacBook-Pro.local>
2019-10-07 16:22:08 -0500

    Initial commit

---
+++ src/main/java/com/yuphanie/ipanena/IpanenaApplication.java
@@ -0,0 +1,13 @@
+package com.yuphanie.ipanena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpanenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpanenaApplication.class, args);
+	}
+
+}
