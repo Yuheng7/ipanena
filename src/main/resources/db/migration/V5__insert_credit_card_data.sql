7f24ad4876493c30855e4a2a27ac36767bc05485
Stephanie Hernandez <hern108@usc.edu>
2019-10-14 11:39:07 -0500

    we got moneeyyy

---
+++ src/main/resources/db/migration/V5__insert_credit_card_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO credit_card (bank, card_name)
+    values ('Bank of America', 'Cash Rewards'),
+    ('Chase', 'Freedom'),
+    ('Chase', 'Sapphire Reserved'),
+    ('American Express', 'Gold Card');
